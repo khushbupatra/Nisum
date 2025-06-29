3f03d06c3e70e6ce972d57b4b4198a5bd42083c2
khushbupatra <155991259+khushbupatra@users.noreply.github.com>
2025-06-30 03:04:32 +0530

    sprint 4 day 2 assignment

---
+++ S4D2/api-cloud-gateway/src/main/java/com/api_cloud_gateway/ApiCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.api_cloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ S4D2/api-cloud-gateway/src/test/java/com/api_cloud_gateway/ApiCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
